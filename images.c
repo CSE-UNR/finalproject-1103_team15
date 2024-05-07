85ee608b71cac7402c2da75d76072ee8a40ae78a
1103-Davis-Batton-Maurice <mdavisbatton@nevada.unr.edu>
2024-05-07 23:04:08 +0000

    we almost have light

--- images.c
+++ images.c
@@ -4,15 +4,15 @@
 
 #include <stdio.h>
 //use these macros for 2D array max sizes
-#define ROWMAX 30
-#define COLMAX 30
+#define ROWMAX 21
+#define COLMAX 21
 
 //function prototypes to go here
 void loadImage(char *fiel, char numDump[][COLMAX]);
 void displayImage(char numDump[][COLMAX]);
 void cropImage();
-void dimImage();
-void brightenImage();
+void dimImage(char numDump[][COLMAX]);
+void brightenImage(char numDump[][COLMAX]);
 void saveImage();
 
 //universal check to see if loadImage was used
@@ -20,7 +20,7 @@ int imageExists = 0;
 
 int main(){
 
-	int mainMenuChoice; 
+	int mainMenuChoice, editMenuChoice; 
 	char imageArray[ROWMAX][COLMAX] = {'\0'};
 	//makes any dead space in a file not fitting the max nonexistent
 	char fileName[50];
@@ -54,7 +54,42 @@ int main(){
 			break;
 			
 			case 3:
-			//probably a function for another menu, leading to editing
+			if(imageExists != 1){
+				printf("\nCan't edit anything if there's nothing to edit!\n");
+				//another loadImage checkpoint
+			}
+			else{
+			printf("\nEDITING MENU\n");
+			printf("1: Crop Image\n");
+			printf("2: Dim Image\n");
+			printf("3: Brighten Image\n");
+			printf("0: Return\n\n");
+			printf("Select one of the above options: ");
+			scanf("%d", &editMenuChoice);
+			
+			switch(editMenuChoice){
+				case 1:
+				printf("In progress");
+				break;
+				
+				case 2:
+				dimImage(imageArray);
+				displayImage(imageArray);
+				break;
+				
+				case 3:
+				brightenImage(imageArray);
+				displayImage(imageArray);
+				break;
+				
+				case 0:
+				break;
+				
+				default:
+				printf("\nThat wasn't a valid input. Returning to main menu.\n");
+				break;
+			}
+			}
 			break;
 			
 			case 0: 
@@ -95,8 +130,29 @@ void loadImage(char *fiel, char numDump[][COLMAX]){
 void displayImage(char numDump[][COLMAX]){
 	for(int x = 0; x < ROWMAX; x++){
 		for(int y = 0; y < COLMAX; y++){
-			printf("%c", numDump[x][y]);
+			switch(numDump[x][y]){
+				case '0':
+				printf(" ");
+				break;
+				
+				case '1':
+				printf(".");
+				break;
+				
+				case '2':
+				printf("o");
+				break;
+				
+				case '3':
+				printf("O");
+				break;
+				
+				case '4':
+				printf("0");
+				break;
+			}
 		}
+		printf("\n");
 	}
 }
 
@@ -104,12 +160,60 @@ void cropImage(){
 
 }
 
-void dimImage(){
-
+void dimImage(char numDump[][COLMAX]){
+	for(int x = 0; x < ROWMAX; x++){
+		for(int y = 0; y < COLMAX; y++){
+			switch(numDump[x][y]){
+				case '0':
+				numDump[x][y] = 'O';
+				break;
+				
+				case 'O':
+				numDump[x][y] = 'o';
+				break;
+				
+				case 'o':
+				numDump[x][y] = '.';
+				break;
+				
+				case '.':
+				numDump[x][y] = ' ';
+				break;
+				
+				case ' ':
+				numDump[x][y] = ' ';
+				break;
+			}
+		}
+	}
 }
 
-void brightenImage(){
-
+void brightenImage(char numDump[][COLMAX]){
+	for(int x = 0; x < ROWMAX; x++){
+		for(int y = 0; y < COLMAX; y++){
+			switch(numDump[x][y]){
+				case ' ':
+				numDump[x][y] = '.';
+				break;
+				
+				case '.':
+				numDump[x][y] = 'o';
+				break;
+				
+				case 'o':
+				numDump[x][y] = 'O';
+				break;
+				
+				case 'O':
+				numDump[x][y] = '0';
+				break;
+				
+				case '0':
+				numDump[x][y] = '0';
+				break;
+			}
+		}
+	}
 }
 
 void saveImage(){
